2b28a0723460e518dfcc5e18f6d6bef4d5d4a9f3
polymahh <otman.elkantaoui@gmail.com>
2023-10-18 20:18:35 +0100

    new project

--- src/reduxToolkit/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import modalReducer from "./features/modalSlice";
-
-export const store = configureStore({
-  reducer: {
-    modal: modalReducer,
-  },
-});
