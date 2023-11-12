ca2b1841572d40e31d92da0982244932b5a52b7e
polymahh <otman.elkantaoui@gmail.com>
2023-11-12 23:29:02 +0100

    finished teams cards

--- components/teamsCard/index.tsx
+++ components/teamsCard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Link from "next/link"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 import { ExternalLink } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
@@ -9,97 +10,175 @@ import { buttonVariants } from "../ui/button"
 
 function TeamsCard() {
   return (
-    <div className="border h-full rounded-lg flex flex-col gap-4 p-4">
+    <div className="border h-full rounded-lg flex flex-col gap-6 p-4">
       <div className="rounded-sm border-b p-1">
         <span className="text-lg font-semibold">Teams:</span>
       </div>
-      <div className="bg-secondary p-4 rounded-md grid grid-cols-3">
+
+      <div className="bg-secondary p-4 rounded-md grid grid-cols-3 ">
+        <div className="flex  gap-4">
+          <Avatar className="h-16 w-16 rounded-sm overflow-hidden">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-4xl">PM</AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="flex gap-1 items-end mb-3">
+              By:
+              <Link
+                className={buttonVariants({
+                  variant: "link",
+                  size: "link",
+                  className: "justify-start",
+                })}
+                href={"/"}
+              >
+                <Icons.gitHub className="h-5 mr-1" />
+                <span>Ekb</span>
+              </Link>
+            </div>
+            <div className="flex gap-2">
+              <span className="bg-red-500 rounded-sm px-2 text-white">
+                Design
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-col gap-2 ">
-          <span>Project repo:</span>
+          <span className="text-sm text-muted-foreground">Project repo:</span>
           <Link
             href={siteConfig.links.github}
             className="bg-secondary-foreground text-secondary flex self-start gap-1 justify-center rounded-sm items-center py-1 px-2"
           >
             <Icons.gitHub className="h-5" />
-            <span>Junior-prep</span> <ExternalLink className="h-4" />
+            <span>Roomates-tinder</span> <ExternalLink className="h-4" />
           </Link>
         </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-1 items-end">
-            By:
-            <Link
-              className={buttonVariants({
-                variant: "link",
-                size: "link",
-                className: "justify-start",
-              })}
-              href={"/"}
-            >
-              <Icons.gitHub className="h-5 mr-1" />
-              <span>PolyMad</span>
-            </Link>
-          </div>
-          <div className="flex gap-2">
-            <span className="bg-sky-500 rounded-sm  px-2 text-white">
-              Frontend
-            </span>
-            <span className="bg-fuchsia-700 rounded-sm  px-2 text-white">
-              Backend
-            </span>
-          </div>
-        </div>
+
         <div className="flex flex-col gap-2 ">
-          <span>Roles needed:</span>
-          <div className="flex gap-2">
+          <span className="text-sm text-muted-foreground">Roles needed:</span>
+          <div className="flex gap-2 flex-wrap">
             <span className="bg-sky-500 rounded-sm  px-2 text-white">
               Frontend
             </span>
             <span className="bg-fuchsia-700 rounded-sm  px-2 text-white">
               Backend
+            </span>
+            <span className="bg-yellow-500 rounded-sm  px-2 text-white">
+              Senior
             </span>
           </div>
         </div>
       </div>
-      <div className="border p-4 rounded-md grid grid-cols-2">
-        <div className="flex flex-col gap-2 ">
-          <span>Project name:</span>
-          <Link
-            href={siteConfig.links.github}
-            className="bg-muted flex self-start gap-1 justify-center rounded-sm items-center py-1 px-2"
-          >
-            <span>Junior-prep</span> <ExternalLink className="h-5 pt-1" />
-          </Link>
+
+      {/* ============ */}
+
+      <div className="bg-secondary p-4 rounded-md grid grid-cols-3">
+        <div className="flex  gap-4">
+          <Avatar className="h-16 w-16 rounded-sm overflow-hidden">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-4xl">PM</AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="flex gap-1 items-end mb-3">
+              By:
+              <Link
+                className={buttonVariants({
+                  variant: "link",
+                  size: "link",
+                  className: "justify-start",
+                })}
+                href={"/"}
+              >
+                <Icons.gitHub className="h-5 mr-1" />
+                <span>Stormix</span>
+              </Link>
+            </div>
+            <div className="flex gap-2">
+              <span className="bg-yellow-500 rounded-sm px-2 text-white">
+                Senior
+              </span>
+              <span className="bg-fuchsia-700 rounded-sm px-2 text-white">
+                Backend
+              </span>
+            </div>
+          </div>
         </div>
         <div className="flex flex-col gap-2 ">
-          <span>Roles needed:</span>
-          <div className="flex gap-2">
+          <span className="text-sm text-muted-foreground">Project repo:</span>
+          <Link
+            href={siteConfig.links.github}
+            className="bg-secondary-foreground text-secondary flex self-start gap-1 justify-center rounded-sm items-center py-1 px-2"
+          >
+            <Icons.gitHub className="h-5" />
+            <span>Omigle-clone</span> <ExternalLink className="h-4" />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-2 ">
+          <span className="text-sm text-muted-foreground">Roles needed:</span>
+          <div className="flex gap-2 flex-wrap">
             <span className="bg-sky-500 rounded-sm  px-2 text-white">
               Frontend
             </span>
-            <span className="bg-fuchsia-700 rounded-sm  px-2 text-white">
-              Backend
+            <span className="bg-red-500 rounded-sm  px-2 text-white">
+              Design
             </span>
           </div>
         </div>
       </div>
-      <div className="border p-4 rounded-md grid grid-cols-2">
-        <div className="flex flex-col gap-2 ">
-          <span>Project name:</span>
-          <Link
-            href={siteConfig.links.github}
-            className="bg-muted flex self-start gap-1 justify-center rounded-sm items-center py-1 px-2"
-          >
-            <span>Junior-prep</span> <ExternalLink className="h-5 pt-1" />
-          </Link>
+
+      {/* ============ */}
+
+      <div className="bg-secondary p-4 rounded-md grid grid-cols-3">
+        <div className="flex  gap-4">
+          <Avatar className="h-16 w-16 rounded-sm overflow-hidden">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-4xl">PM</AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="flex gap-1 items-end mb-3">
+              By:
+              <Link
+                className={buttonVariants({
+                  variant: "link",
+                  size: "link",
+                  className: "justify-start",
+                })}
+                href={"/"}
+              >
+                <Icons.gitHub className="h-5 mr-1" />
+                <span>PolyMad</span>
+              </Link>
+            </div>
+            <div className="flex gap-2">
+              <span className="bg-sky-500 rounded-sm px-2 text-white">
+                Frontend
+              </span>
+              <span className="bg-fuchsia-700 rounded-sm px-2 text-white">
+                Backend
+              </span>
+            </div>
+          </div>
         </div>
         <div className="flex flex-col gap-2 ">
-          <span>Roles needed:</span>
-          <div className="flex gap-2">
-            <span className="bg-sky-500 rounded-sm  px-2 text-white">
-              Frontend
+          <span className="text-sm text-muted-foreground">Project repo:</span>
+          <Link
+            href={siteConfig.links.github}
+            className="bg-secondary-foreground text-secondary flex self-start gap-1 justify-center rounded-sm items-center py-1 px-2"
+          >
+            <Icons.gitHub className="h-5" />
+            <span>Junior-Prep</span> <ExternalLink className="h-4" />
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-2 ">
+          <span className="text-sm text-muted-foreground">Roles needed:</span>
+          <div className="flex gap-2 flex-wrap">
+            <span className="bg-red-500 rounded-sm  px-2 text-white">
+              Design
             </span>
-            <span className="bg-fuchsia-700 rounded-sm  px-2 text-white">
-              Backend
+            <span className="bg-yellow-500 rounded-sm  px-2 text-white">
+              Senior
             </span>
           </div>
         </div>
