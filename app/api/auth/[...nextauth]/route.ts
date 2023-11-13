18f57044ad305d75b063bbaef3fe2873fe8eb5a0
polymahh <otman.elkantaoui@gmail.com>
2023-11-13 18:10:45 +0100

    build fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./authOptions";
+import { authOptions } from '@/lib/authOptions';
+import NextAuth from 'next-auth/next';
 
+const handler = NextAuth(authOptions)!;
 
-
-export const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST}
