94b56b102cb2096a9974236d70b0039b1e9d149d
polymahh <otman.elkantaoui@gmail.com>
2023-12-13 22:03:40 +0100

    feat: cleaning folders and trpc

--- app/_trpc/client.ts
+++
@@ -1,4 +0,0 @@
-import { AppRouter } from "@/trpc";
-import {  createTRPCReact } from "@trpc/react-query";
-
-export const trpc = createTRPCReact<AppRouter>({})
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,11 +2,8 @@
 
 import { ReactNode, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { httpBatchLink } from "@trpc/client"
 import { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
-
-import { trpc } from "@/app/_trpc/client"
 
 function Providers({
   children,
@@ -16,22 +13,11 @@ function Providers({
   session: Session | null
 }) {
   const [queryClient] = useState(() => new QueryClient())
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  )
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider session={session}>{children}</SessionProvider>
-      </QueryClientProvider>
-    </trpc.Provider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </QueryClientProvider>
   )
 }
 
