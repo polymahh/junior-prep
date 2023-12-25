d81b99ad25fb06cc0dee676c658df4220b062d3e
polymahh <otman.elkantaoui@gmail.com>
2023-12-25 22:27:19 +0100

    fix: turning off experimental appdir and switching to built-in-next-font

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
   async redirects() {
     return [
       {
