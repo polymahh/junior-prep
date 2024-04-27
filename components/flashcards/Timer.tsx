ef6f82215ed403592029fcfabc957894eab16c56
polymahh <otman.elkantaoui@gmail.com>
2024-04-27 17:40:57 +0100

    feat: timer logic

--- components/flashcards/Timer.tsx
+++ components/flashcards/Timer.tsx
@@ -1,17 +1,38 @@
 import { useEffect, useState } from "react"
+import { AlarmClock } from "lucide-react"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const SECOND = 1000
 const MINUTE = SECOND * 60
 const HOUR = MINUTE * 60
 
-let timeStart = 10
-
 export const Timer = () => {
-  const [time, setTime] = useState(timeStart)
+  const [time, setTime] = useState(0)
+  const [today, setToday] = useState(new Date().toISOString().slice(0, 10))
+
+  useEffect(() => {
+    const previousTime = localStorage.getItem("time")
+    if (previousTime && previousTime?.split(",")[0] === today) {
+      setTime(parseInt(previousTime.split(",")[1]))
+    } else {
+      setTime(0)
+      localStorage.setItem("time", today + "," + 0)
+    }
+  }, [])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime((prev) => prev + 1)
+      setTime((prev) => {
+        const newTime = prev + 1
+        localStorage.setItem("time", today + "," + newTime.toString())
+        return newTime
+      })
     }, 1000)
 
     return () => clearInterval(interval)
@@ -23,12 +44,23 @@ export const Timer = () => {
   const seconds = time % 60
 
   return (
-    <div className="flex min-w-[106px]">
-      <p>
-        Time: {hours < 10 ? "0" + hours : hours}:
-        {minutes < 10 ? "0" + minutes : minutes}:
-        {seconds < 10 ? "0" + seconds : seconds}
-      </p>
-    </div>
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex min-w-[96px] gap-2 items-center">
+              <AlarmClock className="w-6 h-6" />
+              <p>
+                {hours < 10 ? "0" + hours : hours}:
+                {minutes < 10 ? "0" + minutes : minutes}:
+                {seconds < 10 ? "0" + seconds : seconds}
+              </p>
+            </div>
+          </TooltipTrigger>
+
+          <TooltipContent>Time spent today!</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
   )
 }
